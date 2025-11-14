b6d8d6c1057f17f64e7d36f9cb7dfa44b907b300
PATLO <freedeployment146@gmail.com>
2025-11-14 20:32:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-const express = require('express');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import bodyParser from 'body-parser';
+import server from './qr.js';
+import code from './pair.js';
+import { EventEmitter } from 'events';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const __path = process.cwd();
+
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
+
+EventEmitter.defaultMaxListeners = 500;
+
 app.use('/server', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+
+app.use('/qr', async (req, res, next) => {
+    res.sendFile(__path + '/qr.html');
+});
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
     console.log(`
 Don't Forgot To Give Star
 
- Server running on http://localhost:` + PORT)
-})
+ Server running on http://localhost:` + PORT);
+});
 
-module.exports = app
+export default app;
